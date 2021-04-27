e10c27705b55adb39fd1247e12b3ec07d83c00be
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-27 10:48:27 -0500

    Added SQL statement

--- GamePlayAnalysisI.sql
+++ GamePlayAnalysisI.sql
@@ -66,3 +66,8 @@ VALUES
 (3,4,str_to_date('2018-07-03','%Y-%m-%d'),5);
 
 */
+
+# Write your MySQL query statement below
+select player_id,min(event_date)  first_login
+from Activity
+group by player_id;
