946f22f40e6b60c154a2089d24fa6dc9e656e491
pallabichakraborty <pallavichakraborty@yahoo.com>
2021-04-27 11:10:16 -0500

    Update GamePlayAnalysisIII.sql

--- GamePlayAnalysisIII.sql
+++ GamePlayAnalysisIII.sql
@@ -72,5 +72,5 @@ VALUES
 
 select player_id, 
 		  event_date,
-          sum(games_played) over(partition by player_id order by event_date)
+          sum(games_played) over(partition by player_id order by event_date) games_played_so_far
 from Activity
